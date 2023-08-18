39cf112046d8d757e9b6223f8a767ad5b253528d
nathan <side_nathan@hotmail.com>
2023-08-18 15:15:21 -0400

    Finish exs 3.7-3.8

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 app.use(express.json());
+
+morgan.token("json-body", (req, res) => {
+    return JSON.stringify(req.body);
+});
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :json-body"));
 
 let data = 
 [
