50bd3858c97288a2d17207e1dee5611678040be4
nathan <side_nathan@hotmail.com>
2023-08-19 21:52:19 -0400

    Add validation to name

--- index.js
+++ index.js
@@ -27,17 +27,54 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-    const currentTime = new Date();
-    response.send(`<p>Phonebook has info for ${data.length} people</p>
-                    <p>${currentTime}</p>`)
-})
+    Person.countDocuments()
+        .then(count => {
+            const currentTime = new Date();
+            response.send(`<p>Phonebook has info for ${count} people</p>
+                            <p>${currentTime}</p>`);
+        })
+        .catch(error => {
+            console.error('Error getting document count:', error);
+            response.status(500).send('Error retrieving document count');
+        });
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    };
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson);
+        })
+        .catch(error => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error:'malformatted id'});
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message});
+    }
+
+    next(error);
+}
+
+app.use(errorHandler);
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    data = data.filter(person => person.id !== id);
-
-    response.status(204).end();
-})
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
+});
 
 app.post("/api/persons", (request, response) => {
     const body = request.body;
@@ -51,9 +88,11 @@ app.post("/api/persons", (request, response) => {
         number: body.number,
     });
 
-    person.save().then(savedPerson => {
+    person.save()
+        .then(savedPerson => {
         response.json(savedPerson);
-    })
+        })
+        .catch(error => next(error));
 
 })
 
--- models/person.js
+++ models/person.js
@@ -14,9 +14,13 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
     number: String,
-})
+});
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
